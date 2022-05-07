43548be91fc6dd931fab5cc4173da94602bd0c63
KanoGachi <2324272924@qq.com>
2022-05-07 17:26:37 +0800

    mybatis config fix,add test api

---
+++ src/main/java/test/modules/service/BaseServiceTest.java
@@ -0,0 +1,10 @@
+package test.modules.service;/**
+*@BelongsProject: petshop-back-end
+*@BelongsPackage: test.modules.service
+*@Author: LostKIKI
+*@CreateTime: 2022-05-07  16:33
+*@Description: BaseServiceTest
+*@Version: 1.0
+*/
+public class BaseServiceTest {
+}
